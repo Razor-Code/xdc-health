6a5f7070f8cc6714201349c89c5cb0e0fab4eda2
Rahul.B <talktorahul.b@gmail.com>
2022-09-19 14:36:54 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import { useWeb3 } from "@3rdweb/hooks"
 import Dashboard from '../components/patient/Dashboard/Dashboard'
 import FlashPage from '../components/flashpage/FlashPage'
+import Dialog from '../components/DialogBox/Dialog'
 
 export default function Home() {
   const { address, connectWallet } = useWeb3()
@@ -11,7 +12,7 @@ export default function Home() {
     <div>
     {address ? (
       <>
-     <h1><FlashPage /></h1>
+     <h1><Dialog /></h1>
      </>
      ) : (
         <div onClick= {() => connectWallet('injected')}><h1 className='bg-gray-200 p-10 px-20 rounded-xl text-black max-w-[400px]'>connect</h1></div>
