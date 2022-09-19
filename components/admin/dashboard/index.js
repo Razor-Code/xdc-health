2c673a2fa4d8607a97c364f12efb5f646b5d37e2
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-09-19 21:21:08 +0530

    admin page redesign

---
+++ components/admin/dashboard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "../../sidebar";
+
+export default function dashboard() {
+  let today = new Date();
+  return (
+    <>      
+    <div className="ml-64 p-8 flex flex-col md:items-start items-center">
+      <h1 className="mt-6 text-4xl">Welcome, Admin</h1>
+      <h3>Today is {today.toDateString()} </h3>
+      <Sidebar />
+    </div>
+    </>
+  )
+}
--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,63 +1,52 @@
-export default function Sidebar({visible}) {
+export default function Sidebar() {
   return (
-    <div className={`${visible === 'yes' ? 'block ' : 'hidden '} flex flex-col w-full md:w-64 h-screen px-4 py-8 bg-white fixed dark:bg-gray-900`}>
-      <h2 className="text-3xl font-semibold text-gray-800 dark:text-white">XDC Health</h2>
+    <div className="flex flex-col w-64 h-screen px-4 py-8 absolute top-0 left-0 bg-green-400 border-r dark:bg-gray-200 dark:border-gray-100">
+        <h2 className="text-3xl font-semibold text-gray-800 dark:text-black">XDC Health</h2>
 
-      <div className="relative mt-6">
-          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-              <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                  <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-              </svg>
-          </span>
-
-          <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search" />
-      </div>
-
-      <div className="flex flex-col justify-between flex-1 mt-6">
-          <nav>
-              <a className="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-md dark:bg-gray-800 dark:text-gray-200" href="#">
-                  <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                          d="M19 11H5M19 11C20.1046 11 21 11.8954 21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V13C3 11.8954 3.89543 11 5 11M19 11V9C19 7.89543 18.1046 7 17 7M5 11V9C5 7.89543 5.89543 7 7 7M7 7V5C7 3.89543 7.89543 3 9 3H15C16.1046 3 17 3.89543 17 5V7M7 7H17"
-                          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                  </svg>
-
-                  <span className="mx-4 font-medium">Dashboard</span>
-              </a>
-
-              <a className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                  <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                      <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                  </svg>
-
-                  <span className="mx-4 font-medium">Profile</span>
-              </a>
-
-              <a className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+        <div className="relative mt-6">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
+                    <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
                 </svg>
+            </span>
+
+            <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-200 dark:text-gray-300 dark:border-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search" />
+        </div>
+
+        <div className="flex flex-col justify-between flex-1 mt-6">
+            <nav>
+                <a className="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-md dark:bg-gray-100 dark:text-gray-800" href="#">
+                    <img className="object-cover h-7 w-7 drop-shadow-sm" src="/assets/dashboard.png" alt="avatar" />
+
+                    <span className="mx-2 font-medium">Dashboard</span>
+                </a>
+
+                <a className="flex items-center px-4 py-2 mt-5 text-gray-800 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
+                    <img className="object-cover h-7 w-7 drop-shadow-sm" src="/assets/profile.png" alt="avatar" />
 
 
-                  <span className="mx-4 font-medium">Tickets</span>
-              </a>
+                    <span className="mx-2 font-medium">Profile</span>
+                </a>
 
-              <a className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                </svg>
+                <a className="flex items-center px-4 py-2 mt-5 text-gray-800 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
+                    <img className="object-cover h-7 w-7 drop-shadow-sm" src="/assets/appointment.png" alt="avatar" />
 
+                    <span className="mx-2 font-medium">Appointments</span>
+                </a>
 
-                  <span className="mx-4 font-medium">Documents</span>
-              </a>
-          </nav>
+                <a className="flex items-center px-4 py-2 mt-5 text-gray-800 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
+                    <img className="object-cover h-7 w-7 drop-shadow-sm" src="/assets/document.png" alt="avatar" />
 
-          <div className="flex items-center px-4 -mx-2">
-              <img className="object-cover mx-2 rounded-full h-9 w-9" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar" />
-              <h4 className="mx-2 font-medium text-gray-800 dark:text-gray-200 hover:underline">Org Admin</h4>
-          </div>
-      </div>
-  </div>
+                    <span className="mx-2 font-medium">Documents</span>
+                </a>
+
+                </nav>
+
+            <div className="flex items-center px-4 -mx-2">
+                <img className="object-cover mx-2 rounded-full h-9 w-9" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar" />
+                <h4 className="mx-2 font-medium text-gray-800 dark:text-gray-800 hover:underline">Org Admin</h4>
+            </div>
+        </div>
+    </div>
   )
 }
