142a5bf07856e4940392d44680163ce7fe87d669
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-09-20 14:25:10 +0530

    sidebar completed

--- components/admin/dashboard/index.js
+++ components/admin/dashboard/index.js
@@ -8,7 +8,7 @@ export default function dashboard() {
     <div className="ml-64 p-8 flex flex-col md:items-start items-center">
       <h1 className="mt-6 text-4xl">Welcome, Admin</h1>
       <h3>Today is {today.toDateString()} </h3>
-      <Sidebar />
+      <Sidebar page="dashboard"/>
     </div>
     </>
   )
--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,7 +1,19 @@
-export default function Sidebar() {
+const Sidelinks = (props) => {
+    return (
+        <a className={props.current ? "flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-md mt-5" : "flex items-center px-4 py-2 mt-5 text-gray-200 transition-colors duration-300 transform rounded-md hover:bg-white hover:bg-opacity-30 hover:text-gray-700"} href="#">
+            <img className="object-cover h-7 w-7 drop-shadow-sm" src={props.image} alt="nav link img" />
+
+            <span className="mx-2 font-medium">{props.name}</span>
+        </a>
+    )
+}
+
+
+
+export default function Sidebar(props) {
   return (
-    <div className="flex flex-col w-64 h-screen px-4 py-8 absolute top-0 left-0 bg-green-400 border-r dark:bg-gray-200 dark:border-gray-100">
-        <h2 className="text-3xl font-semibold text-gray-800 dark:text-black">XDC Health</h2>
+    <div className="flex flex-col w-64 h-screen px-4 py-8 absolute top-0 left-0 border-r bg-teal-700">
+        <h2 className="text-3xl font-semibold text-white">XDC Health</h2>
 
         <div className="relative mt-6">
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
@@ -10,41 +22,20 @@ export default function Sidebar() {
                 </svg>
             </span>
 
-            <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-200 dark:text-gray-300 dark:border-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search" />
+            <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring" placeholder="Search" />
         </div>
 
-        <div className="flex flex-col justify-between flex-1 mt-6">
+        <div className="flex flex-col justify-between flex-1 mt-3">
             <nav>
-                <a className="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-md dark:bg-gray-100 dark:text-gray-800" href="#">
-                    <img className="object-cover h-7 w-7 drop-shadow-sm" src="/assets/dashboard.png" alt="avatar" />
-
-                    <span className="mx-2 font-medium">Dashboard</span>
-                </a>
-
-                <a className="flex items-center px-4 py-2 mt-5 text-gray-800 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                    <img className="object-cover h-7 w-7 drop-shadow-sm" src="/assets/profile.png" alt="avatar" />
-
-
-                    <span className="mx-2 font-medium">Profile</span>
-                </a>
-
-                <a className="flex items-center px-4 py-2 mt-5 text-gray-800 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                    <img className="object-cover h-7 w-7 drop-shadow-sm" src="/assets/appointment.png" alt="avatar" />
-
-                    <span className="mx-2 font-medium">Appointments</span>
-                </a>
-
-                <a className="flex items-center px-4 py-2 mt-5 text-gray-800 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                    <img className="object-cover h-7 w-7 drop-shadow-sm" src="/assets/document.png" alt="avatar" />
-
-                    <span className="mx-2 font-medium">Documents</span>
-                </a>
-
-                </nav>
+                <Sidelinks name="Dashboard" image="/assets/dashboard.png" current={props.page == "dashboard" ? true : false} />
+                <Sidelinks name="Profile" image="/assets/profile.png" current={props.page == "profile" ? true : false} />
+                <Sidelinks name="Appointments" image="/assets/appointment.png" current={props.page == "appoinments" ? true : false} />   
+                <Sidelinks name="Documents" image="/assets/document.png" current={props.page == "documents" ? true : false} />
+            </nav>
 
             <div className="flex items-center px-4 -mx-2">
                 <img className="object-cover mx-2 rounded-full h-9 w-9" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar" />
-                <h4 className="mx-2 font-medium text-gray-800 dark:text-gray-800 hover:underline">Org Admin</h4>
+                <h4 className="mx-2 font-medium text-gray-200 hover:underline">Org Admin</h4>
             </div>
         </div>
     </div>
